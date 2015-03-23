2c503c80602de9a8d62706e11dac5e79b3cfb298
Akshay Jagtiani <ajagtian@usc.edu>
2015-03-23 01:11:33 -0700

    Mar_23_1_11_AM

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 // ® Riyusaki
 (function(){
-var app_uri = 'http://riyusaki.com'
+var app_uri = 'http://riyusaki.com/'
 var db_uri = "http://54.191.81.102:8000/update/";
 var cookieName = 'randomID';
 var appDomain = 'riyusaki.com';
@@ -11,80 +11,89 @@ var url_focus_time_map = {};
 var tab_id_url_map = {};
 var __aT__ = null;
 var __aTi__ = null;
+var prefName = '_ryski'
+var cookieName = 'randomID'
+
 riyusaki = {
+  setCookie:function(){
+    chrome.cookies.get({url:app_uri, name:cookieName}, function(cookie){
+      if(cookie == null || cookie == undefined){
+        chrome.storage.local.get('_ryski', function(result){
+          if (result._ryski == null || result._ryski == undefined){
+            var randomID = parseInt(Math.random()*10000000+1).toString();
+            chrome.storage.local.set({'_ryski':randomID});
+            var expiry = ((new Date()).getTime()/1000) + 86400;
+            chrome.cookies.set({url:app_uri, name:cookieName, value:randomID, expirationDate:expiry});
+          }
+          else {
+            var expiry = ((new Date()).getTime()/1000) + 86400;
+            chrome.cookies.set({url:app_uri, name:cookieName, value:result._ryski, expirationDate:expiry});
+          }
+        });
+      }
+    });
+  },
   init : function(){
-  /*  chrome.tabs.onActivated.addListener(function (activeInfo) {
-      chrome.tabs.getSelected(null, function(tab){
-        var tab_url = (tab.url).toString();
-        //riyusaki.log('activated - '+url)
-        // riyusaki.log("activated: "+url);
-        var tab_id = (tab.id).toString();
-        if(tab_id != __aTi__) {
-          var tab_title = tab.title.toString();
+    chrome.webRequest.onBeforeRequest.addListener(function(details){
+      riyusaki.setCookie();
+    }, {urls:[app_uri]});
+    chrome.tabs.onActivated.addListener(function (activeInfo) {
+      chrome.tabs.getSelected(null, function(tab_1){
+        var tab_url = String(tab_1.url);
+
+        var tab_id = String(tab_1.id);
+        var tab_title = String(tab_1.title);
+        if(tab_id != __aTi__ && tab_title.match(pornTags)) {
           riyusaki.recordTitle(tab_url, tab_title);
           riyusaki.recordID(tab_id, tab_url);
-
-          // riyusaki.log("__aT__ before: "+__aT__);
           if(!riyusaki.isEmpty(__aT__) && __aT__ != tab_url){
             riyusaki.discontinueLogger(__aT__);
-            __aT__ = tab_url;
-            __aTi__ = tab_id;
+            __aT__ = String(tab_url);
+            __aTi__ = String(tab_id);
             riyusaki.continueLogger(__aT__);
            }
           if(riyusaki.isEmpty(__aT__)){
-            __aT__ = tab_url;
-            __aTi__ = tab_id;
+            __aT__ = String(tab_url);
+            __aTi__ = String(tab_id);
             riyusaki.continueLogger(tab_url);
           }
         }
-
        });
-    });*/
-    chrome.tabs.onUpdated.addListener(function (tabid, changeInfo, tab) {
-      var tab_url = (tab.url).toString();
-      //riyusaki.log('updated - '+url)
-      // riyusaki.log("updated: "+url);
-      var tab_title = (tab.title).toString();
-      if(tab_title.toLowerCase().indexOf("new tab") == -1){
-        //riyusaki.recordTitle(tab_url, tab_title);
-        //riyusaki.recordID(tabid.toString(), tab_url);
+    });
+    chrome.tabs.onUpdated.addListener(function (tabid, changeInfo, tab2) {
+      chrome.tabs.getSelected(null, function(tab){
+        if(tab.title.toLowerCase().indexOf("new tab") == -1 && tab.title.match(pornTags)){
+          riyusaki.recordTitle(tab.url, tab.title);
+          riyusaki.recordID(String(tabid), tab.url);
 
-        if (!riyusaki.isEmpty(__aT__) && __aT__ == tab_url){
-          // do nothing
+          if (!riyusaki.isEmpty(__aT__) && __aT__ == tab.url){
+          }
+          if(!riyusaki.isEmpty(__aT__) && __aT__ != tab.url){
+            riyusaki.stopLogger(__aT__);
+            __aT__ = String(tab.url);
+            __aTi__ = String(tabid);
+            riyusaki.continueLogger(__aT__)
+          }
+          if(riyusaki.isEmpty(__aT__)){
+            __aT__ = String(tab.url);
+            __aTi__ = String(tabid);
+            riyusaki.continueLogger(__aT__);
+          }
         }
-        if(!riyusaki.isEmpty(__aT__) && __aT__ != tab_url){
-          //riyusaki.stopLogger(last_active_tab);
-          __aT__ = tab_url;
-          __aTi__ = tabid;
-          //riyusaki.continueLogger(__aT__)
-        }
-        // riyusaki.log("__aT__ before: "+__aT__);
-        //
-        if(riyusaki.isEmpty(__aT__)){
-          __aT__ = tab_url;
-          __aTi__ = tabid;
-          //riyusaki.continueLogger(__aT__);
-        }
-        //
-        //riyusaki.log("__aT__ after: "+__aT__);
-        //riyusaki.log("record_id "+tabid+"=>"+tab_url);
+      });
+    });
+    chrome.tabs.onRemoved.addListener(function (tabID, removeInfo) {
+      if(!riyusaki.isEmpty(tab_id_url_map[tabID])){
+        riyusaki.stopLogger(tab_id_url_map[tabID]);
       }
     });
-
-  /*  chrome.tabs.onRemoved.addListener(function (tabID, removeInfo) {
-        //riyusaki.log("before remove: "+tab_id_url_map[tabID]);
-
-        riyusaki.stopLogger(tab_id_url_map[tabID]);
-        //riyusaki.log("removed: "+tab_id_url_map[tabID]);
-        //__aT__ = null;
-        //riyusaki.log('removed')
-    }); */
-
+    chrome.windows.onRemoved.addListener(function (windowID){
+      riyusaki.destroyData();
+    });
   },
-/*  recordTitle:function(url, title){
+  recordTitle:function(url, title){
     if(riyusaki.isEmpty(url_title_map[url])){
       url_title_map[url] = title;
-    //  riyusaki.log("recordTitle:"+url);
     }
   },
   recordID:function(id, url){
@@ -96,54 +105,46 @@ riyusaki = {
     url_focus_time_map[url] = new Date().getTime();
     if(riyusaki.isEmpty(url_time_map[url]))
       url_time_map[url] = 0;
-    //riyusaki.log("continueLogger:"+url);
   },
   discontinueLogger:function(url){
     url_time_map[url] += parseInt((new Date().getTime() - url_focus_time_map[url])/1000);
     url_focus_time_map[url] = new Date().getTime();
-    //riyusaki.log("discontinueLogger:"+url);
   },
   stopLogger:function(url){
     url_time_map[url] += parseInt( (new Date().getTime() - url_focus_time_map[url]) /1000);
     url_focus_time_map[url] = new Date().getTime();
-    done = false;
-    riyusaki.record(url, url_title_map[url], url_time_map[url], true);
-
-    //riyusaki.log("stopLogger:"+url);
-
+    var done = false;
+    done = riyusaki.record(url, url_title_map[url], url_time_map[url]);
+    if (done == true){
+      riyusaki.delete_data(url)
+    }
   },
   destroyData:function(){
-
-  },*/
+    for (url in url_time_map){
+      riyusaki.stopLogger(url)
+    }
+    delete tab_id_url_map;
+  },
   isEmpty:function(obj){
     if(obj == null || obj == undefined || obj == "")
       return true;
     else
       return false;
-  }/*,
-  record:function(url, title, time, del){
+  },
+  record:function(url, title, time){
+    if(!riyusaki.isEmpty(url) && !riyusaki.isEmpty(title) && time != NaN)
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open("GET", "http://localhost:5000/log/"+url+'/'+time+'/'+title, true);
     xmlHttp.send();
-    xmlHttp.onreadystatechange = function(){
-      if(xmlHttp.status == 200 && xmlHttp.readyState == 4 && del){
-        riyusaki.delete_data(url)
-        return true;
-      }
-    }
+    return true
 
   },
-  log:function(text){
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", "http://localhost:5000/log/"+text, true);
-    xmlHttp.send();
-    return true;
-  },
-  delete_data : function(url){
+  delete_data:function(url){
     delete url_title_map[url];
     delete url_time_map[url];
     delete url_focus_time_map[url];
-  }*/
+  }
 }
+riyusaki.setCookie();
 riyusaki.init();
 })();
